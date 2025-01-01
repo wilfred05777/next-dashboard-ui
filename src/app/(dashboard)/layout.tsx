5d4d7cd9ffa8e6d5d55276c049fe61551721a3fd
Wilfred Erdo Bancairen <wilfred05777@gmail.com>
2025-01-01 08:14:48 +0800

    adjustment ignore

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -13,10 +13,10 @@ export default function DashboardLayout({
     <div className = "h-screen flex">
       {/* LEFT */}
       {/* {children} */}
-      <div className="w-[14%] md:w-[8%] lg:w-[14%] xl:w-[14%] p-4">
+      <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] p-4">
         <Link href="/" className="flex items-center justify-center gap-2 lg:justify-start">
           <Image src="/logo.png" alt="logo" width={32} height={32}/>
-          <span className="hidden lg:block">School Management System</span>
+          <span className="hidden lg:block font-bold">ERDO School</span>
         </Link>
         <Menu/>
       </div>
