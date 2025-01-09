ff7291a4853ec9ea85811f165ab05513fbc3eee9
Wilfred Erdo Bancairen <wilfred05777@gmail.com>
2025-01-09 17:58:32 +0800

    menu and data.ts dummy data to test user role, add menu hover effect - 1:23:00 end

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,6 +1,7 @@
 import Link from 'next/link';
 import Image from 'next/image';
 import React from 'react'
+import { role } from '@/lib/data';
 
 
 const menuItems = [
@@ -125,14 +126,26 @@ const Menu = () => {
       {menuItems.map((i) => (
         <div className='flex flex-col gap-2' key={i.title}>
           <span className='hidden lg:block text-gray-400 font-light my-4'>{i.title}</span>
-          {
+          {i.items.map((item) => {
+            if (item.visible.includes(role)) {
+              return (
+                <Link href={item.href} key={item.label} className='flex items-center justify-center lg:justify-start gap-4 text-gray-500 py-2 md:px-2 rounded-md hover:bg-erdoSkyLight'>
+                  <Image src={item.icon} alt="" width={20} height={20} />
+                  <span className='hidden lg:block'>{item.label}</span>
+                </Link>
+              )
+            }
+          })}
+
+          {/* {
             i.items.map(item => (
               <Link href={item.href} key={item.label} className='flex items-center justify-center lg:justify-start gap-4 text-gray-500 py-2'>
                 <Image src={item.icon} alt="" width={20} height={20} />
                 <span className='hidden lg:block'>{item.label}</span>
               </Link>
             ))
-          }
+          } */}
+
         </div>
       ))}
     </div>
