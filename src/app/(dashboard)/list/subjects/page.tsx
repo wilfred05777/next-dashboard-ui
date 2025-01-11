27a82dfeed316098dde63f86059b0fd6cf0be7ee
Wilfred Erdo Bancairen <wilfred05777@gmail.com>
2025-01-11 19:37:51 +0800

    list @ teachers 1:49:14 end

--- src/app/(dashboard)/list/subjects/page.tsx
+++ src/app/(dashboard)/list/subjects/page.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const SubjectsListPage = () => {
   return (
-    <div>SubjectsListPage</div>
+      <div className=''>SubjectsListPage
+        
+      </div>
   )
 }
 
--- src/app/(dashboard)/list/teachers/page.tsx
+++ src/app/(dashboard)/list/teachers/page.tsx
@@ -1,8 +1,30 @@
+import TableSearch from '@/components/TableSearch'
+import Image from 'next/image'
 import React from 'react'
 
 const TeacherListPage = () => {
   return (
-    <div>TeacherListPage</div>
+    <div className='container h-fit bg-white p-4 rounded-md flex-1 m-4 mt-0'>
+          {/* Teachers Pages */}
+          {/* TOP */}
+          <div className="flex items-center justify-between">
+              <h1 className="hidden md:block text-lg font-semibold">All Teachers</h1>
+              <div className="flex flex-col md:flex-row items-center gap-4 w-full md:w-auto" >
+                  <TableSearch />
+                  <div className="flex">
+                      <button>
+                          <Image src="/filter.png" alt='' width={14} height={14} />
+                      </button>
+                  </div>
+              </div>
+          </div>
+
+          {/* LIST */}
+          <div className="flex"></div>
+
+          {/* Pagination */}
+          <div className="flex"></div>
+    </div>
   )
 }
 
