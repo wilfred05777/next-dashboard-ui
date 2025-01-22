84d04853602719d1c7b81ef42c6d42656ac65908
Wilfred Erdo Bancairen <wilfred05777@gmail.com>
2025-01-22 08:44:23 +0800

    Subject list page - 2:20:03 -end

--- src/app/(dashboard)/list/subjects/page.tsx
+++ src/app/(dashboard)/list/subjects/page.tsx
@@ -1,11 +1,85 @@
-import React from 'react'
+import React from "react";
+import Pagination from "@/components/Pagination";
+import Table from "@/components/Table";
+import TableSearch from "@/components/TableSearch";
+import Image from "next/image";
+import Link from "next/link";
+import { role, subjectsData } from "@/lib/data";
 
-const SubjectsListPage = () => {
-  return (
-      <div className=''>SubjectsListPage
-        
+type Subject = {
+   id: number;
+   name: string;
+   teachers: string[]; // string[] array kay example: one subject may naa sa teachers/students lists
+};
+
+const columns = [
+   {
+      header: "Subject Name",
+      accessor: "name",
+      // classname: "hidden md:table-cell"
+   },
+   {
+      header: "Teachers",
+      accessor: "teachers",
+      classname: "hidden md:table-cell",
+   },
+   {
+      header: "Actions",
+      accessor: "actions",
+      // classname: "hidden lg:table-cell"
+   },
+];
+
+const SubjectListPage = () => {
+   const renderRow = (item: Subject) => (
+      <tr
+         key={item.id}
+         className='border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-erdoPurpleLight'
+      >
+         <td className='flex items-center gap-4 p-4'>{item.name}</td>
+         <td className='hidden md:table-cell'>{item.teachers.join(", ")}</td>
+         <td>
+            <div className='flex items-center gap-2'>
+               <Link href={`/list/Students/${item.id}`}>
+                  <button className='w-7 h-7 flex items-center justify-center rounded-full bg-erdoSky'>
+                     <Image src='/view.png' alt='' width={16} height={16} />
+                  </button>
+               </Link>
+               {role === "admin" && (
+                  <button className='w-7 h-7 flex items-center justify-center rounded-full bg-erdoPurple'>
+                     <Image src='/delete.png' alt='' width={16} height={16} />
+                  </button>
+               )}
+            </div>
+         </td>
+      </tr>
+   );
+
+   return (
+      <div className='container h-full bg-white p-4 rounded-md flex-1 m-4 mt-0'>
+         {/* Students Pages */}
+         {/* TOP */}
+         <div className='flex items-center justify-between'>
+            <h1 className='hidden md:block text-lg font-semibold'>
+               All Subjects
+            </h1>
+            <div className='flex flex-col md:flex-row items-center gap-4 w-full md:w-auto'>
+               <TableSearch />
+               <div className='flex'>
+                  <button>
+                     <Image src='/filter.png' alt='' width={14} height={14} />
+                  </button>
+               </div>
+            </div>
+         </div>
+
+         {/* LIST */}
+         <Table columns={columns} renderRow={renderRow} data={subjectsData} />
+
+         {/* Pagination */}
+         <Pagination />
       </div>
-  )
-}
+   );
+};
 
-export default SubjectsListPage
+export default SubjectListPage;
